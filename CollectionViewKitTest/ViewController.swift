afcab3ed28912e0cd1cb5e54ac926e7b447056f1
luboolu <kjjy98@naver.com>
2022-04-22 13:47:18 +0900

    Add 버튼 그림자 추가

--- CollectionViewKitTest/ViewController.swift
+++ CollectionViewKitTest/ViewController.swift
@@ -65,6 +65,11 @@ class ViewController: UIViewController {
     private func setUI() {
         addButton.clipsToBounds = true
         addButton.layer.cornerRadius = 10
+        addButton.layer.shadowColor = UIColor.black.cgColor // 색깔
+        addButton.layer.masksToBounds = false
+        addButton.layer.shadowOffset = CGSize(width: 4, height: 4) // 위치조정
+        addButton.layer.shadowRadius = 5 // 반경
+        addButton.layer.shadowOpacity = 0.3 // alpha값
         
         deleteButton.clipsToBounds = true
         deleteButton.layer.cornerRadius = 10
