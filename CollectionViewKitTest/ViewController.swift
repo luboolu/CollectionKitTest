65795f90a9fa35865194558036910393dd8c44b2
jykim <jykim@difine.co.kr>
2022-04-22 09:14:30 +0900

    first commit

--- CollectionViewKitTest/ViewController.swift
+++ CollectionViewKitTest/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CollectionKit
+import SnapKit
 
+class ViewController: UIViewController {
+    
+    let collectionView = CollectionView()
+
+    @IBOutlet weak var BackGroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        BackGroundView.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalToSuperview().offset(14)
+        }
+        
+        let dataSource = ArrayDataSource(data: Array(1...10))
+        let viewSource = ClosureViewSource(viewUpdater: { (view: UILabel, data: Int, index: Int) in
+            
+            view.backgroundColor = .systemBlue
+            view.text = "\(data)"
+            view.textAlignment = .center
+            view.clipsToBounds = true
+            view.layer.cornerRadius = 5
+            view.gestureRecognizers = [UIGestureRecognizer(target: self, action: #selector(self.cellDidTapped))]
+        })
+        let sizeSource = { (index: Int, data: Int, collectionSize: CGSize) -> CGSize in
+          return CGSize(width: 50, height: 50)
+        }
+        let provider = BasicProvider(
+          dataSource: dataSource,
+          viewSource: viewSource,
+          sizeSource: sizeSource
+        )
+        
+        provider.layout = FlowLayout(spacing: 10)
+
+        //lastly assign this provider to the collectionView to display the content
+        collectionView.provider = provider
+        
+    }
+    
+    @objc func cellDidTapped() {
+        print(#function)
     }
 
 
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        
+    }
 }
 
